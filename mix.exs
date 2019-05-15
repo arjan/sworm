91e409a50f1b4b6fdd36ba8d0a27cda9f62411b4
Arjan Scherpenisse <arjan@scherpenisse.net>
2019-05-15 13:52:40 +0200

    Only support Elixir 1.7 and up

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sworm.MixProject do
     [
       app: :sworm,
       version: File.read!("VERSION"),
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
